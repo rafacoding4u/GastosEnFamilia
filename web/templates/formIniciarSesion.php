2e41b4b5b691c39be407c3cba50a0a58b024a7d6
rafacoding4u <rafmurgom@alu.edu.gva.es>
2024-09-08 16:35:44 +0200

    Cambios a aplicación más sencilla GastosEnFamilia

--- web/templates/formIniciarSesion.php
+++ web/templates/formIniciarSesion.php
@@ -1,32 +1,35 @@
 <?php ob_start() ?>
 
-<div class="container text-center p-4">
-    <div class="col-md-12" id="cabecera">
-        <h1 class="h1Inicio">TIENDA VIRTUAL</h1>
-    </div>
-</div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card mt-5">
+                <div class="card-header text-center">
+                    <h2>Iniciar Sesión</h2>
+                </div>
+                <div class="card-body">
+                    <?php if (isset($params['mensaje'])) : ?>
+                        <div class="alert alert-danger">
+                            <?php echo $params['mensaje'] ?>
+                        </div>
+                    <?php endif; ?>
 
-<div class="container text-center py-2">
-    <div class="col-md-12">
-        <?php if (isset($params['mensaje'])) : ?>
-            <b><span style="color: rgba(200, 119, 119, 1);"><?php echo $params['mensaje'] ?></span></b>
-        <?php endif; ?>
+                    <form action="index.php?ctl=iniciarSesion" method="post">
+                        <div class="form-group mb-3">
+                            <label for="nombreUsuario">Nombre de Usuario</label>
+                            <input type="text" class="form-control" id="nombreUsuario" name="nombreUsuario" placeholder="Nombre de usuario">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="contrasenya">Contraseña</label>
+                            <input type="password" class="form-control" id="contrasenya" name="contrasenya" placeholder="Contraseña">
+                        </div>
+                        <button type="submit" class="btn btn-primary w-100" name="bIniciarSesion">Aceptar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-
-<div class="container text-center p-4">
-    <form action="index.php?ctl=iniciarSesion" method="post" name="formIniciarSesion">
-        <h5><b>Iniciar sesión</b></h5>
-        <p><input type="text" name="nombreUsuario" placeholder="Nombre de usuario"><br></p>
-        <p><input type="password" name="contrasenya" placeholder="Contraseña"><br></p>
-        <input type="submit" name="bIniciarSesion" value="Aceptar"><br>
-    </form>
 </div>
 
 <?php $contenido = ob_get_clean() ?>
-
 <?php include 'layout.php' ?>
-
-
-
-
--- web/templates/formRegistro.php
+++ web/templates/formRegistro.php
@@ -1,42 +1,59 @@
 <?php ob_start() ?>
 
-<div class="container text-center p-4">
-    <div class="col-md-12" id="cabecera">
-        <h1 class="h1Inicio">REGISTRARSE</h1>
-    </div>
-</div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card mt-5">
+                <div class="card-header text-center">
+                    <h2>Registro de Usuario</h2>
+                </div>
+                <div class="card-body">
+                    <?php if (isset($params['mensaje'])) : ?>
+                        <div class="alert alert-info">
+                            <?php echo $params['mensaje'] ?>
+                        </div>
+                    <?php endif; ?>
+                    
+                    <?php if (!empty($errores)) : ?>
+                        <div class="alert alert-danger">
+                            <?php foreach ($errores as $error) {
+                                echo $error . "<br>";
+                            } ?>
+                        </div>
+                    <?php endif; ?>
 
-<div class="container text-center py-2">
-    <div class="col-md-12">
-        <?php if (isset($params['mensaje'])) : ?>
-            <b><span style="color: rgba(200, 119, 119, 1);"><?php echo $params['mensaje'] ?></span></b>
-        <?php endif; ?>
+                    <form action="index.php?ctl=registro" method="post">
+                        <div class="form-group mb-3">
+                            <label for="nombre">Nombre</label>
+                            <input type="text" class="form-control" id="nombre" name="nombre" value="<?php echo $params['nombre'] ?>" placeholder="Nombre">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="apellido">Apellido</label>
+                            <input type="text" class="form-control" id="apellido" name="apellido" value="<?php echo $params['apellido'] ?>" placeholder="Apellido">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="nombreUsuario">Nombre de Usuario</label>
+                            <input type="text" class="form-control" id="nombreUsuario" name="nombreUsuario" value="<?php echo $params['nombreUsuario'] ?>" placeholder="Nombre de usuario">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="contrasenya">Contraseña</label>
+                            <input type="password" class="form-control" id="contrasenya" name="contrasenya" value="<?php echo $params['contrasenya'] ?>" placeholder="Contraseña">
+                            <small class="form-text text-muted">La contraseña debe contener al menos 1 letra mayúscula, 1 número y tener un tamaño mínimo de 8 caracteres.</small>
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="nivel_usuario">Nivel de Usuario</label>
+                            <select class="form-select" id="nivel_usuario" name="nivel_usuario">
+                                <option value="1" <?php if ($params['nivel_usuario'] == 1) echo 'selected'; ?>>Usuario</option>
+                                <option value="2" <?php if ($params['nivel_usuario'] == 2) echo 'selected'; ?>>Administrador</option>
+                            </select>
+                        </div>
+                        <button type="submit" class="btn btn-primary w-100" name="bRegistro">Aceptar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="col-md-12">
-        <?php foreach ($errores as $error) { ?>
-            <b><span style="color: rgba(200, 119, 119, 1);"><?php echo $error . "<br>"; ?></span></b>
-        <?php } ?>
-    </div>
-</div>
-
-<div class="container text-center p-1">
-<form action="index.php?ctl=registro" method="post" name="formRegistro">
-    <p>* <input type="text" name="nombre" value="<?php echo $params['nombre'] ?>" placeholder="Nombre"> <br></p>
-    <p>* <input type="text" name="apellido" value="<?php echo $params['apellido'] ?>" placeholder="Apellido"><br></p>
-    <p>* <input type="text" name="nombreUsuario" value="<?php echo $params['nombreUsuario'] ?>" placeholder="Nombre de usuario"><br></p>
-    <p>* <input type="password" name="contrasenya" value="<?php echo $params['contrasenya'] ?>" placeholder="Contraseña"><br></p>
-    <p><small>La contraseña debe contener al menos 1 letra mayúscula, 1 número y tener un tamaño mínimo de 8 caracteres.</small></p>
-    <p>* <select name="nivel_usuario">
-        <option value="1">Usuario</option>
-        <option value="2">Administrador</option>
-    </select></p>
-    <input type="submit" name="bRegistro" value="Aceptar"><br>
-</form>
 </div>
 
 <?php $contenido = ob_get_clean() ?>
-
 <?php include 'layout.php' ?>
-
-
-
